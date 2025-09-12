77fe9291357d16b82e6468561066a2c30eddcc6f
MataclysmGames <matterickson915@gmail.com>
2025-09-12 11:30:01 -0700

    menu, font, logo

---
+++ scripts/menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var play_button: Button = %PlayButton
+@onready var settings_button: Button = %SettingsButton
+@onready var quit_button: Button = %QuitButton
+
+func _ready() -> void:
+	play_button.pressed.connect(on_play_button)
+	settings_button.pressed.connect(on_setttings_button)
+	quit_button.pressed.connect(on_quit_button)
+
+func on_play_button() -> void:
+	pass
+	#get_tree().change_scene_to_file()
+
+func on_setttings_button() -> void:
+	pass
+	#get_tree().change_scene_to_file()
+
+func on_quit_button() -> void:
+	get_tree().quit()
